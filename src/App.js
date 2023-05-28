4e4afb20557e80238692f91106f196cc65edaf4c
Walyson Felix <walysonfelix1991@gmail.com>
2023-05-28 09:23:01 -0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import CryptoJS from "crypto-js";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [screen, setScreen] = useState("encrypt");
+
+  const [encrptedData, setEncrptedData] = useState("");
+  const [decrptedData, setDecrptedData] = useState("");
+
+  const secretPass = "XkhZG4fW2t2W";
+
+  const encryptData = () => {
+    const data = CryptoJS.AES.encrypt(
+      JSON.stringify(text),
+      secretPass
+    ).toString();
+
+    setEncrptedData(data);
+  };
+
+  const decryptData = () => {
+    const bytes = CryptoJS.AES.decrypt(text, secretPass);
+    const data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    setDecrptedData(data);
+  };
+
+  const switchScreen = (type) => {
+    setText("");
+    setEncrptedData("");
+    setDecrptedData("");
+    setScreen(type);
+  };
+
+  const handleClick = () => {
+    if (!text) return;
+
+    if (screen === "encrypt") encryptData();
+    else decryptData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div>
+        <button
+          className="btn btn-left"
+          style={{
+            backgroundColor: screen === "encrypt" ? "#5e35b1" : "#5e35b130",
+          }}
+          onClick={() => {
+            switchScreen("encrypt");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Encrypt
+        </button>
+
+        <button
+          className="btn btn-right"
+          style={{
+            backgroundColor: screen === "decrypt" ? "#1e88e5" : "#1e88e530",
+          }}
+          onClick={() => {
+            switchScreen("decrypt");
+          }}
+        >
+          Decrypt
+        </button>
+      </div>
+
+      <div className="card">
+        <input
+          value={text}
+          onChange={({ target }) => {
+            setText(target.value);
+          }}
+          name="text"
+          type="text"
+          placeholder={
+            screen === "encrypt" ? "Enter Text" : "Enter Encrypted Data"
+          }
+        />
+
+        <button className="btn submit-btn" onClick={handleClick}>
+          {screen === "encrypt" ? "Encrypt" : "Decrypt"}
+        </button>
+      </div>
+
+      {encrptedData || decrptedData ? (
+        <div className="content">
+          <label>{screen === "encrypt" ? "Encrypted" : "Decrypted"} Data</label>
+          <p>{screen === "encrypt" ? encrptedData : decrptedData}</p>
+        </div>
+      ) : null}
     </div>
   );
 }
 
 export default App;
+
